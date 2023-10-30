fa52c57ea9add6e1f1c918bbac115bfcece85929
Vasil Evgeniev Aleksandrov <vvvaleksandrovv@gmail.com>
2023-10-30 13:36:56 +0200

    adding ports

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require("express");
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const request = require('request')
 const Blockchain = require("./blockchain");
-const PubSub = require('./pubsub')
+const PubSub = require('./pubsub');
 
 const app = express();
 const blockchain = new Blockchain();
 const pubsub = new PubSub({blockchain});
 
-setTimeout(() => {pubsub.broadcastChain()}, 1000)
+const DEFAULT_PORT = 3000;
+let PEER_PORT;
 
+
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`
 app.use(bodyParser.json());
 
 app.get("/api/blocks", (req, res) => {
@@ -20,12 +24,31 @@ app.post("/api/mine", (req, res) => {
 
   blockchain.addBlock({ data });
 
-
+  pubsub.broadcastChain();
 
   res.redirect('/api/blocks');
 });
 
-const PORT = 3000;
+const syncChains = () => {
+  request({url: `${ROOT_NODE_ADDRESS}/api/blocks`}, (error,response,body) => {
+    if(!error && response.statusCode === 200) {
+      const rootChain = JSON.parse(body);
+
+      blockchain.replaceChain(rootChain);
+
+      console.log('replace chain on a sync with ', rootChain)
+    }
+  })
+};
+
+if(process.env.GENERATE_PEER_PORT === 'true') {
+  PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000)
+}
+
+const PORT = PEER_PORT || DEFAULT_PORT
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
+  if(PORT !== DEFAULT_PORT) {
+    syncChains();
+  }
 });
